2236192f1ca321e9a0b99a1a05fdb833152e2dbf
Leaf26 <lxgoldsmith@gmail.com>
2021-09-14 04:34:45 -0400

    husktowns and saberfactions

---
+++ src/main/java/leafcraft/rtp/tools/softdepends/HuskTownsChecker.java
@@ -0,0 +1,13 @@
+package leafcraft.rtp.tools.softdepends;
+
+import me.william278.husktowns.HuskTownsAPI;
+
+public class HuskTownsChecker {
+    public static Boolean isInClaim(org.bukkit.Location location) {
+        try {
+            return !HuskTownsAPI.getInstance().isWilderness(location);
+        } catch (NoClassDefFoundError e) {
+            return false;
+        }
+    }
+}
