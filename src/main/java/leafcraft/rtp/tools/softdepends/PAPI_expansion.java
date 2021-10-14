8b23cb8b9c97f3187869326b9992b04a40ecc4bf
Leaf26 <lxgoldsmith@gmail.com>
2021-10-14 14:55:30 -0400

    fix duplicate locations and console messages

--- src/main/java/leafcraft/rtp/tools/softdepends/PAPI_expansion.java
+++ src/main/java/leafcraft/rtp/tools/softdepends/PAPI_expansion.java
@@ -49,7 +49,7 @@ public class PAPI_expansion extends PlaceholderExpansion{
 
 	@Override
     public @NotNull String getVersion(){
-        return "1.3.13";
+        return "1.3.14";
     }
 
     @Override
