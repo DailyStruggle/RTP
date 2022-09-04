48e8fb05b85ed78ef414d9a58c4aa826a6f35fcf
Leaf26 <lxgoldsmith@gmail.com>
2022-09-04 03:23:14 -0400

    integrations addon

---
+++ addons/RTP-ClaimPluginIntegrations/src/main/java/io/github/dailystruggle/softdepends/FactionsChecker.java
@@ -0,0 +1,15 @@
+package io.github.dailystruggle.softdepends;
+
+import com.massivecraft.factions.Board;
+import com.massivecraft.factions.FLocation;
+
+public class FactionsChecker {
+    public static Boolean isInClaim(org.bukkit.Location location) {
+        try {
+            FLocation fLocation = new FLocation(location);
+            return Board.getInstance().getFactionAt(fLocation).getOwnerList(fLocation).size()>0;
+        } catch (NoClassDefFoundError | NullPointerException e) {
+            return false;
+        }
+    }
+}
