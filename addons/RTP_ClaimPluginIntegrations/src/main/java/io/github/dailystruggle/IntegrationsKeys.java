fbeb616bf3f757d804a2cafced4dd04e50e3f3b6
Leaf26 <lxgoldsmith@gmail.com>
2022-09-05 03:04:30 -0400

    works

---
+++ addons/RTP_ClaimPluginIntegrations/src/main/java/io/github/dailystruggle/IntegrationsKeys.java
@@ -0,0 +1,12 @@
+package io.github.dailystruggle;
+
+public enum IntegrationsKeys {
+    rerollWorldGuard,
+    rerollGriefDefender,
+    rerollGriefPrevention,
+    rerollTownyAdvanced,
+    rerollHuskTowns,
+    rerollFactions,
+    rerollLands,
+    rerollRedProtect
+}
