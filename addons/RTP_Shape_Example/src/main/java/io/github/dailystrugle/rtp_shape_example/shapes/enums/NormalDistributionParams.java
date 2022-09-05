6ca743c93eee2b69e7d707549dff9336aa75868e
Leaf26 <lxgoldsmith@gmail.com>
2022-09-05 03:04:30 -0400

    works

---
+++ addons/RTP_Shape_Example/src/main/java/io/github/dailystrugle/rtp_shape_example/shapes/enums/NormalDistributionParams.java
@@ -0,0 +1,12 @@
+package io.github.dailystrugle.rtp_shape_example.shapes.enums;
+
+public enum NormalDistributionParams {
+    radius,
+    centerRadius,
+    centerX,
+    centerZ,
+    uniquePlacements,
+    mean,
+    deviation,
+    expand
+}
