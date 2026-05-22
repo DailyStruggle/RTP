4142b4431e888122720d134ef6c4ec7e5b32b7f0
Leaf26 <lxgoldsmith@gmail.com>
2026-05-21 20:02:42 -0500

    extraneous file cleanup

--- commands-api/tmp_brigadier/com/mojang/brigadier/exceptions/CommandExceptionType.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-package com.mojang.brigadier.exceptions;
-
-public interface CommandExceptionType {
-}
