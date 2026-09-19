1ba5f80239f6101a2ff7a91e35255e45bb36fe6d
Leaf26 <lxgoldsmith@gmail.com>
2026-09-19 01:39:09 -0500

    Revert "correct group feature location"
    This reverts commit df976139848b05cc6bfd3b1ae60868e831043c39.

--- addons/LeafRTPGroupAddon/src/test/java/io/github/dailystruggle/rtp/groupaddon/GroupMultiConfigTest.java
+++ addons/LeafRTPGroupAddon/src/test/java/io/github/dailystruggle/rtp/groupaddon/GroupMultiConfigTest.java
@@ -1,5 +1,6 @@
 package io.github.dailystruggle.rtp.groupaddon;
 
+import io.github.dailystruggle.rtp.common.RTP;
 import io.github.dailystruggle.rtp.common.configuration.ConfigParser;
 import io.github.dailystruggle.rtp.common.configuration.MultiConfigParser;
 import io.github.dailystruggle.rtp.common.mock.RTPTestSetup;
--- rtp-core/src/main/java/io/github/dailystruggle/rtp/common/commands/menu/MenuMirrorSubcommand.java
+++ rtp-core/src/main/java/io/github/dailystruggle/rtp/common/commands/menu/MenuMirrorSubcommand.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 /**
  * Shadow {@link TreeCommand} mirroring a node from the live {@code /rtp} tree under {@link MenuRedeemSubcommand}.
